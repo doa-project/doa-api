f3ee8ed1a5f0343a8eb25a67acc56e46e1189843
sophiekumagai <sophie.satie20@gmail.com>
2024-07-28 15:21:34 -0300

    adicionando o projeto de api do doa

---
+++ doa-api-project/src/test/java/org/example/doaapiproject/DoaApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.doaapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoaApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
